3e7a1f034e83b9d555e120508f956517bc3d3306
seqhilla <57414546+seqhilla@users.noreply.github.com>
2021-01-16 21:28:18 +0300

    Add files via upload

---
+++ src/models/Dersler.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace seqhilla\akademisyenders\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "dersler".
+ *
+ * @property int $id
+ * @property int|null $dersID
+ * @property string|null $dersSaatleri
+ * @property string|null $dersGunleri
+ * @property string|null $dersAdi
+ * @property string|null $ogrtUyesiAdi
+ */
+class Dersler extends \yii\db\ActiveRecord
+{
+    /**
+     * {@inheritdoc}
+     */
+    public static function tableName()
+    {
+        return 'dersler';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [
+            //[['dersID'], 'integer'],
+            [['dersID','dersSaatleri', 'dersGunleri', 'dersAdi', 'ogrtUyesiAdi'], 'string', 'max' => 255],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'dersID' => 'Ders Kodu',
+            'dersSaatleri' => 'Ders Saatleri',
+            'dersGunleri' => 'Ders Günleri',
+            'dersAdi' => 'Ders Adı',
+            'ogrtUyesiAdi' => 'Akademisyen',
+        ];
+    }
+
+    public function getOgrtuye() {
+        return $this->hasone(Ogrtuye::class,['ogrtUyesiID'=>'ogrtUyesiAdi']);
+    }
+
+
+
+}
---
+++ src/models/Ogrtuye.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace seqhilla\akademisyenders\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "ogrtuye".
+ *
+ * @property int $id
+ * @property string|null $ogrtUyesiID
+ * @property string|null $ogrtUyesiFakultesi
+ * @property string|null $ogrtUyesiAdi
+ * @property string|null $verdigiDersKodlari
+ * @property string|null $verdigiDersKodlari2
+ * @property string|null $verdigiDersKodlari3
+ * @property string|null $verdigiDersKodlari4
+ */
+class Ogrtuye extends \yii\db\ActiveRecord
+{
+    /**
+     * {@inheritdoc}
+     */
+    public static function tableName()
+    {
+        return 'ogrtuye';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [
+            [['ogrtUyesiID', 'ogrtUyesiFakultesi', 'ogrtUyesiAdi', 'verdigiDersKodlari', 'verdigiDersKodlari2', 'verdigiDersKodlari3', 'verdigiDersKodlari4'], 'string', 'max' => 255],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'ogrtUyesiID' => 'Akademisyen ID',
+            'ogrtUyesiFakultesi' => 'Fakultesi',
+            'ogrtUyesiAdi' => 'Akademisyenin Adı ve Soyadı',
+            'verdigiDersKodlari' => 'Verdigi Dersler',
+            'verdigiDersKodlari2' => '',
+            'verdigiDersKodlari3' => '',
+            'verdigiDersKodlari4' => '',
+        ];
+    }
+}
