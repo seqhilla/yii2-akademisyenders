fcf6cc6fc589899f0a7b5bdfc7b589a9f838c857
seqhilla <57414546+seqhilla@users.noreply.github.com>
2021-01-17 13:57:51 +0300

    Add files via upload
    Yanlış dizine koyulmuş o düzeltildi
    içerisindeki bir dizin hatası giderildi

--- src/models/DerslerSearch.php
+++ src/models/DerslerSearch.php
@@ -4,7 +4,7 @@ namespace seqhilla\akademisyenders\models;
 
 use yii\base\Model;
 use yii\data\ActiveDataProvider;
-use common\models\Dersler;
+use seqhilla\akademisyenders\models\Dersler;
 
 /**
  * DerslerSearch represents the model behind the search form of `common\models\Dersler`.
--- src/models/OgrtuyeSearch.php
+++ src/models/OgrtuyeSearch.php
@@ -4,7 +4,7 @@ namespace seqhilla\akademisyenders\models;
 
 use yii\base\Model;
 use yii\data\ActiveDataProvider;
-use common\models\Ogrtuye;
+use seqhilla\akademisyenders\models\Ogrtuye;
 
 /**
  * OgrtuyeSearch represents the model behind the search form of `common\models\Ogrtuye`.
