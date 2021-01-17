c9b3217d11e4b4125d4309de26b7a90bcb94732f
seqhilla <57414546+seqhilla@users.noreply.github.com>
2021-01-17 13:49:11 +0300

    Add files via upload
    Problem çözüldü artık elle taşımaya gerek yok.

--- src/models/Ogrtuye.php
+++ src/models/Ogrtuye.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace common\models;
+namespace seqhilla\akademisyenders\models;
 
 use Yii;
 
--- src/models/OgrtuyeSearch.php
+++ src/models/OgrtuyeSearch.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace common\models;
+namespace seqhilla\akademisyenders\models;
 
 use yii\base\Model;
 use yii\data\ActiveDataProvider;
